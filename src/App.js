93c6007184dbbba6dc43107e1783ea7a65c6b083
RosheedatOyindamola <Rosheedat1234@yahoo>
2023-04-19 16:54:35 +0100

    Build starwars film

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import StarWars from "../src/Components/Star-Wars"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StarWars />
+    </div>
+  );
+}
+
+export default App;
